3865f3db199d39b3b2da76c2ed9015ce964d1fd3
K22 <ulrichkageu@gmail.com>
2023-12-10 13:48:10 +0100

    serveur 01 avec html et erro

--- server.js
+++ server.js
@@ -6,15 +6,23 @@ let server = http.createServer() //Cree server comme istance de http
 server.on('request', (request, reponce) =>{  //Notre fontion
 
     fs.readFile('index.html', (err, data) =>{ // "err => prend en paramettre erro" "data =>prend en paramettre la page a lir"
-        if (err) throw err
+        if (err) {
+           /* reponce.writeHead(404)
+            reponce.end('ERROR 404')*/
+            fs.readFile('erro.html', (err1, data1) =>{
+                reponce.writeHead(404)
+            reponce.end(data1)
+            })
+        }
+        else{
+            reponce.writeHead(200, {
+                'content-type' : 'text/html ;  charset =utf-8' 
+            })
+            reponce.end(data)
         
-    reponce.writeHead(200, {
-        'content-type' : 'text/html ;  charset =utf-8' 
+        }
+        
     })
-    reponce.end(data)
-
-    })
-
 })
 server.listen(8080) //Execute notre serveur sur le port 8080
 
