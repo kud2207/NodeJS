59fb477bd45083c826f5adce4e3e252d66804688
K22 <ulrichkageu@gmail.com>
2023-12-10 12:43:26 +0100

    ini serveur

--- server.js
+++ server.js
@@ -1,2 +1,13 @@
-let http  = require('http')
-let server = http.createServer()
+let http  = require('http') //Initie HTTP dans la variable http
+
+let server = http.createServer() //Cree server comme istance de http
+
+server.on('request', (request, reponce) =>{  //Notre fontion
+    reponce.writeHead(200, {
+        'content-type' : 'text/html ;  charset =utf-8' 
+    })
+    reponce.end('salut boss éé ?')
+
+})
+server.listen(8080) //Execute notre serveur sur le port 8080
+
